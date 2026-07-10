b5bde12580255994ae9c8ead540ee502bce41ce0
croffle2001-ship-it <croffle2001@gmail.com>
2026-07-10 17:11:26 +0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -60,3 +60,10 @@ export interface ShopConfig {
   closeTime: string;
 }
 
+export interface ShopConfig {
+  isOpen: boolean;
+  autoSchedule: boolean;
+  openTime: string;
+  closeTime: string;
+  qrCodeUrl?: string; // เพิ่มบรรทัดนี้เข้ามา
+}
