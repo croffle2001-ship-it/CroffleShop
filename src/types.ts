d7550577eabbe7252423160b2db5c5e6af97a656
croffle2001-ship-it <croffle2001@gmail.com>
2026-07-11 12:15:44 +0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -60,3 +60,16 @@ export interface ShopConfig {
   closeTime: string;
 }
 
+export interface Order {
+  id: string;
+  customerName: string;
+  phone: string;
+  roomNo: string;
+  items: any[];
+  totalPrice: number;
+  status: OrderStatus;
+  createdAt: string;
+  note?: string;
+  deliveryPhoto?: string;
+  paymentSlipUrl?: string; // 👈 เพิ่มบรรทัดนี้
+}
